bc2777b13fe7058177719eb02ad3d59a34d4082f
rheeze <ggybzz2297@gmail.com>
2024-11-28 15:44:00 -0500

    complete a very big sum exercise

--- AVeryBigSum/AVeryBigSum.java
+++ AVeryBigSum/AVeryBigSum.java
@@ -1,3 +1,4 @@
+package AVeryBigSum;
 import java.io.*;
 import java.math.*;
 import java.security.*;
@@ -21,9 +22,12 @@ class Result {
 
     public static long aVeryBigSum(List<Long> ar) {
     // Write your code here
-
+    long sum = 0;
+    for(int i = 0; i < ar.size(); i++) {
+        sum += ar.get(i);
     }
-
+    return sum;
+    }
 }
 
 public class AVeryBigSum {
@@ -31,7 +35,7 @@ public class AVeryBigSum {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
 
-        int arCount = Integer.parseInt(bufferedReader.readLine().trim());
+        bufferedReader.readLine().trim();
 
         List<Long> ar = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
             .map(Long::parseLong)
--- JavaOutputFormatting/JavaOutputFormatting.java
+++ JavaOutputFormatting/JavaOutputFormatting.java
@@ -1,3 +1,4 @@
+package JavaOutputFormatting;
 import java.util.Scanner;
 
 public class JavaOutputFormatting {

9c2ea577a491f5828bdbd00c5f265d602cab6e21
rheeze <ggybzz2297@gmail.com>
2024-11-28 18:23:55 -0500

    complete staircase exercise

--- Staircase/Staircase.java
+++ Staircase/Staircase.java
@@ -21,9 +21,22 @@ class Result {
 
     public static void staircase(int n) {
     // Write your code here
-
+        for(int i = 0; i < n; i++)
+        {
+            for(int j = 0; j < n; j++)
+            {
+                if(j < n - 1 - i)
+                {
+                    System.out.print(" ");
+                }
+                else
+                {
+                    System.out.print("#");
+                }
+            }
+            System.out.println("");
+        }
     }
-
 }
 
 public class Staircase {

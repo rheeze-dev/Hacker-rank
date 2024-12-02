b3da78191aa8f9886a682b47591a625d82ced322
rheeze-dev <ggybzz2297@gmail.com>
2024-12-01 23:57:42 -0500

    template game of stones exercise

---
+++ GameOfStones/GameOfStones.java
@@ -0,0 +1,53 @@
+package GameOfStones;
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'gameOfStones' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts INTEGER n as parameter.
+     */
+
+    public static String gameOfStones(int n) {
+    // Write your code here
+
+    }
+
+}
+
+public class GameOfStones {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int t = Integer.parseInt(bufferedReader.readLine().trim());
+
+        IntStream.range(0, t).forEach(tItr -> {
+            try {
+                int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+                String result = Result.gameOfStones(n);
+
+                bufferedWriter.write(result);
+                bufferedWriter.newLine();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}

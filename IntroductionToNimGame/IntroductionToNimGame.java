a577ab0a28fe3381534fcf76c8a0354fbef543ac
rheeze-dev <ggybzz2297@gmail.com>
2024-12-02 22:07:19 -0500

    template introduction to nim game exercise

---
+++ IntroductionToNimGame/IntroductionToNimGame.java
@@ -0,0 +1,62 @@
+package IntroductionToNimGame;
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'nimGame' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts INTEGER_ARRAY pile as parameter.
+     */
+
+    public static String nimGame(List<Integer> pile) {
+    // Write your code here
+        if(pile.size() % 2 == 0) {
+            return "Second";
+        }
+        else {
+            return "First";
+        }
+    }
+
+}
+
+public class IntroductionToNimGame {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int g = Integer.parseInt(bufferedReader.readLine().trim());
+
+        IntStream.range(0, g).forEach(gItr -> {
+            try {
+                bufferedReader.readLine().trim();
+
+                List<Integer> pile = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                    .map(Integer::parseInt)
+                    .collect(toList());
+
+                String result = Result.nimGame(pile);
+
+                bufferedWriter.write(result);
+                bufferedWriter.newLine();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}

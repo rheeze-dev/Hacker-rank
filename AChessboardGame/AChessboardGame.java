884be4cebd5a365b77ab2d093be3f78b24185eaa
rheeze-dev <ggybzz2297@gmail.com>
2024-12-02 22:03:41 -0500

    template a chessboard game exercise

---
+++ AChessboardGame/AChessboardGame.java
@@ -0,0 +1,59 @@
+package AChessboardGame;
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'chessboardGame' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts following parameters:
+     *  1. INTEGER x
+     *  2. INTEGER y
+     */
+
+    public static String chessboardGame(int x, int y) {
+    // Write your code here
+
+    }
+
+}
+
+public class AChessboardGame {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int t = Integer.parseInt(bufferedReader.readLine().trim());
+
+        IntStream.range(0, t).forEach(tItr -> {
+            try {
+                String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+                int x = Integer.parseInt(firstMultipleInput[0]);
+
+                int y = Integer.parseInt(firstMultipleInput[1]);
+
+                String result = Result.chessboardGame(x, y);
+
+                bufferedWriter.write(result);
+                bufferedWriter.newLine();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
